aed676f3b26394e0e23b1930f41cb74518280b9f
JCly-rikiu <jcly.rikiu@gmail.com>
2017-10-21 15:50:36 +0800

    setup develop environment for HanLPhttp

---
+++ NER/HanLPhttp/src/ntu/ltcl/hanlp/HanLPhttp.java
@@ -0,0 +1,11 @@
+package ntu.ltcl.hanlp;
+
+import com.hankcs.hanlp.HanLP;
+
+public class HanLPhttp {
+
+  public static void main(String[] args) {
+    System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
+  }
+
+}
